4733073f35d46bd1812cf8e01c6ce349193e11d5
Vidrul <Vidrul@ugithub.com>
2020-05-07 12:55:09 +0300

    Initial Commit

---
+++ ChatApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp2
+//
+//  Created by David Saley on 5/7/20.
+//  Copyright © 2020 David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
